8f0dd2b8c4cb96c131b18d491951dbced8563a9e
Valentin Ochs <a@0au.de>
2012-09-05 20:54:58 +0200

    sort includes

--- carrotd.c
+++ carrotd.c
@@ -1,15 +1,16 @@
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <pthread.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 #include "marcov/marcov.h"
 
 #define EVERRRRRRRR (;;)
